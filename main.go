e7fbc87b265990a02ecaa10456f59e94370fd3fb
Timothy <yexiaozhou2003@gmail.com>
2017-09-06 10:19:28 +0800

    init project

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func query(word string) {
+	doc, err := goquery.NewDocument(fmt.Sprintf("http://dict.youdao.com/w/%s", word))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the result
+	result := doc.Find("div#phrsListTab > div.trans-container > ul").Text()
+	fmt.Println(result)
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		displayUsage()
+		os.Exit(0)
+	}
+
+	word := os.Args[1]
+	query(word)
+}
---
+++ utils.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/fatih/color"
+
+const (
+	logo = `
+██╗   ██╗██████╗ ██╗ ██████╗████████╗
+╚██╗ ██╔╝██╔══██╗██║██╔════╝╚══██╔══╝
+ ╚████╔╝ ██║  ██║██║██║        ██║   
+  ╚██╔╝  ██║  ██║██║██║        ██║   
+   ██║   ██████╔╝██║╚██████╗   ██║   
+   ╚═╝   ╚═════╝ ╚═╝ ╚═════╝   ╚═╝   
+
+YDict V0.1 
+https://github.com/TimothyYe/ydict
+
+`
+)
+
+func displayUsage() {
+	color.Cyan(logo)
+	color.Cyan("Usage: ydict [word]")
+}
