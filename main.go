ae3d3288948eb8d9a0d17645be6658add0b37def
Timothy <yexiaozhou2003@gmail.com>
2019-10-11 18:47:52 +0800

    integrate with cobra

--- main.go
+++ main.go
@@ -1,43 +1,71 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
 	"github.com/TimothyYe/ydict/lib"
+	"github.com/spf13/cobra"
+)
+
+var (
+	withVoice int
+	withMore  bool
+	withCache bool
+	isQuiet   bool
+	withReset bool
 )
 
 func main() {
 	//Check & load .env file
 	lib.LoadEnv()
 
-	if len(os.Args) == 1 {
-		lib.DisplayUsage()
-		os.Exit(0)
+	if len(os.Args) == 1 ||
+		(len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "help")) {
+		lib.DisplayLogo()
 	}
 
-	if len(os.Args) == 2 && os.Args[1] == "-h" {
-		lib.DisplayUsage()
-		os.Exit(0)
+	var rootCmd = &cobra.Command{
+		Use: "ydict",
+		Run: func(cmd *cobra.Command, args []string) {
+			if args[0] == "help" {
+				if err := cmd.Usage(); err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				return
+			}
+
+			if withReset {
+				lib.ClearCahceFiles()
+				return
+			}
+
+			queryP := lib.QueryParam{}
+			queryP.Words = args
+			queryP.WordString = strings.Join(args, " ")
+			queryP.WithMore = withMore
+			queryP.WithCache = withCache
+			queryP.IsQuiet = isQuiet
+			queryP.IsMulti = (len(args) > 1)
+			queryP.IsChinese = lib.IsChinese(queryP.WordString)
+			queryP.WithVoice = withVoice
+			if !lib.IsAvailableOS() {
+				queryP.WithVoice = 0
+			}
+			queryP.DoQuery()
+		},
 	}
 
-	words, withVoice, withMore, isQuiet, withCache, clearCache := lib.ParseArgs(os.Args[1:])
-	if clearCache {
-		lib.ClearCahceFiles()
-		return
-	}
+	rootCmd.PersistentFlags().IntVarP(&withVoice, "voice", "v", 0, "Query with voice speech, the default voice play count is 1.")
+	rootCmd.PersistentFlags().BoolVarP(&withMore, "more", "m", false, "Query with more example sentences.")
+	rootCmd.PersistentFlags().BoolVarP(&withCache, "cache", "c", false, "Query with local cache, and save the query word(s) into the cache.")
+	rootCmd.PersistentFlags().BoolVarP(&withReset, "reset", "r", false, "Clear all the words from the local cache.")
+	rootCmd.PersistentFlags().BoolVarP(&isQuiet, "quiet", "q", false, "Query with quiet mode, don't show spinner.")
 
-	queryP := lib.QueryParam{}
-	queryP.Words = words
-	queryP.WordString = strings.Join(words, " ")
-	queryP.WithMore = withMore
-	queryP.WithCache = withCache
-	queryP.IsQuiet = isQuiet
-	queryP.IsMulti = (len(words) > 1)
-	queryP.IsChinese = lib.IsChinese(queryP.WordString)
-	queryP.WithVoice = withVoice
-	if !lib.IsAvailableOS() {
-		queryP.WithVoice = 0
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	queryP.DoQuery()
 }
